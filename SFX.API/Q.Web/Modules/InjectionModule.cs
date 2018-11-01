f15988280c8f1ca08a85d9d6aa6c12550e74f107
dvsmart <36995044+dvsmart@users.noreply.github.com>
2018-11-01 19:52:10 +0000

    ioc changes

--- SFX.API/Q.Web/Modules/InjectionModule.cs
+++ SFX.API/Q.Web/Modules/InjectionModule.cs
@@ -1,29 +1,95 @@
 ﻿using System.Reflection;
 using Autofac;
+using Microsoft.Extensions.DependencyInjection;
 using SFX.Domain;
+using SFX.Domain.Assessment;
+using SFX.Domain.Asset;
+using SFX.Domain.Common;
+using SFX.Domain.CustomEntity;
+using SFX.Domain.Event;
+using SFX.Domain.Menu;
+using SFX.Domain.Task;
+using SFX.Domain.User;
 using SFX.Infrastructure;
 using SFX.Infrastructure.Mappings;
+using SFX.Services.Interfaces.Assessment;
+using SFX.Services.Interfaces.Asset.Properties;
+using SFX.Services.Interfaces.Authentication;
+using SFX.Services.Interfaces.CustomEntity;
+using SFX.Services.Interfaces.Event;
+using SFX.Services.Interfaces.Menu;
+using SFX.Services.Interfaces.Settings.CustomEntityManagement;
+using SFX.Services.Interfaces.Task;
+using SFX.Services.Interfaces.User;
+using SFX.Services.Service.Assessment;
+using SFX.Services.Service.Asset.Properties;
+using SFX.Services.Service.Authentication;
+using SFX.Services.Service.CustomEntity;
+using SFX.Services.Service.Event;
+using SFX.Services.Service.Menu;
+using SFX.Services.Service.Settings.CustomEntityManagement;
+using SFX.Services.Service.Task;
+using SFX.Services.Service.User;
 using SFX.Web.Controllers.Asset;
 using SFX.Web.Mappings;
 
 namespace SFX.Web.Modules
 {
-    public class InjectionModule : Autofac.Module
+    public static class InjectionModule 
     {
-        protected override void Load(ContainerBuilder builder)
+        public static void ConfigureServices(IServiceCollection services)
         {
-            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>));
+            services.AddSingleton<IAuthenticationService, AuthenticationService>();
+            services.AddSingleton<IJwtTokenHelper, JwtTokenHelper>();
 
-            builder.RegisterAssemblyTypes(Assembly.Load("SFX.Services")).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
+            //Repository
+            services.AddTransient<IGenericRepository<AssessmentScope>, GenericRepository<AssessmentScope>>();
+            services.AddTransient<IGenericRepository<Assessment>, GenericRepository<Assessment>>();
+            services.AddTransient<IGenericRepository<AssessmentStatus>, GenericRepository<AssessmentStatus>>();
+            services.AddTransient<IGenericRepository<AssessmentType>, GenericRepository<AssessmentType>>();
+            services.AddTransient<IGenericRepository<AssetProperty>, GenericRepository<AssetProperty>>();
+            services.AddTransient<IGenericRepository<Asset>, GenericRepository<Asset>>();
+            services.AddTransient<IGenericRepository<CustomFieldType>, GenericRepository<CustomFieldType>>();
+            services.AddTransient<IGenericRepository<CustomEntityGroup>, GenericRepository<CustomEntityGroup>>();
+            services.AddTransient<IGenericRepository<CustomField>, GenericRepository<CustomField>>();
+            services.AddTransient<IGenericRepository<CustomEntityInstance>, GenericRepository<CustomEntityInstance>>();
+            services.AddTransient<IGenericRepository<CustomEntity>, GenericRepository<CustomEntity>>();
+            services.AddTransient<IGenericRepository<CustomFieldValue>, GenericRepository<CustomFieldValue>>();
+            services.AddTransient<IGenericRepository<CustomEntityGroup>, GenericRepository<CustomEntityGroup>>();
+            services.AddTransient<IGenericRepository<CustomTab>, GenericRepository<CustomTab>>();
+            services.AddTransient<IGenericRepository<Event>, GenericRepository<Event>>();
+            services.AddTransient<IGenericRepository<MenuItem>, GenericRepository<MenuItem>>();
+            services.AddTransient<IGenericRepository<MenuGroup>, GenericRepository<MenuGroup>>();
+            services.AddTransient<IGenericRepository<RecurrenceType>, GenericRepository<RecurrenceType>>();
+            services.AddTransient<IGenericRepository<TaskPriority>, GenericRepository<TaskPriority>>();
+            services.AddTransient<IGenericRepository<Task>, GenericRepository<Task>>();
+            services.AddTransient<IGenericRepository<TaskStatus>, GenericRepository<TaskStatus>>();
+            services.AddTransient<IGenericRepository<User>, GenericRepository<User>>();
+            services.AddTransient<IGenericRepository<UserRole>, GenericRepository<UserRole>>();
 
-            builder.RegisterAssemblyTypes(typeof(IOutputConverter).Assembly)
-                .AsImplementedInterfaces()
-                .InstancePerLifetimeScope();
+            //Services
+            services.AddTransient<IOutputConverter, OutputConverter>();
+            services.AddTransient<IAssessmentScopeService, AssessmentScopeService>();
+            services.AddTransient<IAssessmentService, AssessmentService>();
+            services.AddTransient<IAssessmentStatusService, AssessmentStatusService>();
+            services.AddTransient<IAssessmentTypeService, AssessmentTypeService>();
+            services.AddTransient<IAssetPropertyService, AssetPropertyService>();
+            services.AddTransient<IFormControlTypeService, FormControlTypeService>();
+            services.AddTransient<ITemplateCategoryService, TemplateCategoryService>();
+            services.AddTransient<ITemplateFormControlService, TemplateFormControlService>();
+            services.AddTransient<ITemplateFormRecordService, TemplateFormRecordService>();
+            services.AddTransient<ITemplateFormValueService, TemplateFormValueService>();
+            services.AddTransient<ITemplateService, TemplateService>();
+            services.AddTransient<ITemplateTabService, TemplateTabService>();
+            services.AddTransient<IEventService, EventService>();
+            services.AddTransient<IMenuService, MenuService>();
+            services.AddTransient<ICustomEntityManagementService, CustomEntityManagementService>();
+            services.AddTransient<ICustomTemplateService, CustomTemplateService>();
+            services.AddTransient<ITaskPriorityService, TaskPriorityService>();
+            services.AddTransient<ITaskService, TaskService>();
+            services.AddTransient<ITaskStatusService, TaskStatusService>();
+            services.AddTransient<IUserService, UserService>();
 
-            builder.RegisterAssemblyTypes(typeof(OutputConverter).Assembly)
-                .AsImplementedInterfaces().InstancePerLifetimeScope();
-
-            builder.RegisterAssemblyTypes(typeof(Presenter).Assembly).AsSelf().InstancePerLifetimeScope();
         }
     }
 }
--- SFX.API/Q.Web/Startup.cs
+++ SFX.API/Q.Web/Startup.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using SFX.Infrastructure.Context;
-using SFX.Services.Interfaces.Authentication;
-using SFX.Services.Interfaces.User;
-using SFX.Services.Service.Authentication;
 using SFX.Web.Filters;
 using SFX.Web.Helpers;
 using SFX.Web.Modules;
@@ -32,7 +23,7 @@ namespace SFX.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddCors(options =>
@@ -56,16 +47,9 @@ namespace SFX.Web
             services.AddDbContext<AppDbContext>(options => options.UseLazyLoadingProxies().UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
             services.AddSwaggerDocumentation();
-            services.AddSingleton<IAuthenticationService, AuthenticationService>();
-            services.AddSingleton<IJwtTokenHelper,JwtTokenHelper>();
-
+            InjectionModule.ConfigureServices(services);
             services.AddJwtAuthentication(Configuration);
-            var builder = new ContainerBuilder();
-            builder.RegisterModule(new InjectionModule());
-            builder.Populate(services);
-            ApplicationContainer = builder.Build();
-           
-            return new AutofacServiceProvider(ApplicationContainer);
+             
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
